879ef165efcb68ff0c4061666be89ffc93ae3b95
ARISTO CRAT <aristakrat31@gmail.com>
2020-05-02 18:47:14 +0500

    Login and Register pages are updated

---
+++ resources/views/templates/js/login_register.js
@@ -0,0 +1,54 @@
+var app = angular.module('login_register_app', []);
+app.controller('login_register_controller',
+    function ($scope, $http) {
+        $scope.closeMsg = function () {
+            $scope.alertMsg = false;
+        };
+
+        $scope.login_form = true;
+
+        $scope.showRegister = function () {
+            $scope.alertMsg = false;
+            $http.get("/register");
+        };
+
+        $scope.showLogin = function () {
+            $scope.alertMsg = false;
+            $http.get("/login")
+        };
+
+        $scope.submitRegister = function () {
+            $http({
+                method: "POST",
+                url: "register.php",
+                data: $scope.registerData
+            }).success(function (data) {
+                $scope.alertMsg = true;
+                if (data.error !== '') {
+                    $scope.alertClass = 'alert-danger';
+                    $scope.alertMessage = data.error;
+                } else {
+                    $scope.alertClass = 'alert-success';
+                    $scope.alertMessage = data.message;
+                    $scope.registerData = {};
+                }
+            });
+        };
+
+        $scope.submitLogin = function () {
+            $http({
+                method: "POST",
+                url: "login.php",
+                data: $scope.loginData
+            }).success(function (data) {
+                if (data.error !== '') {
+                    $scope.alertMsg = true;
+                    $scope.alertClass = 'alert-danger';
+                    $scope.alertMessage = data.message;
+                } else {
+                    location.reload();
+                }
+            });
+        };
+
+    });
