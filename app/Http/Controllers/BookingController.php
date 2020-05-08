62e24527b0223dce358a42c48955cd9668b87321
Farrukhbek <farruhzokirov00@gmail.com>
2020-05-08 19:09:33 +0500

    fixed dome issues with routes

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -6,39 +6,11 @@ use Illuminate\Http\Request;
 use App\Booking;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use App\User;
 
 class BookingController extends Controller
 {
-    //get
-    public function getBooking()
-    {
-        return view('home.room_order');
-    }
 
-    public function bookingRoom(Request $req)
-    {
-    	$user = Auth::user();
-
-    	dd($user);
-
-
-    	$booking = new Booking([
-    		'arrival' => $req->input('arrival'),
-    		'book_time' => $req->input('book_time'),
-    		'checkout' => $req->input('checkout'),
-    		'breakfast' =>$req->input('breakfast'),
-    		'night' => $req->input('night'),
-    		'comment' => $req->input('comment')
-    	]);
-
-    	$user->bookings()->save($booking);
-
-    	return redirect()->route('Index');
-
-    	// return redirect()->route('Index')->with([
-    	// 	// 'info'=>'Your booked a room successfully! We will wait you in '. $req->input('arrival')
-    	// ]);
-    }
-
+    
     
 }
