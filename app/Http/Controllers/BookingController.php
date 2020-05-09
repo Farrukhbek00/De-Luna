65ac900d242dab3a615a37d0dd0bd3e3b34e6803
masrur1999 <bekmirzayev.m@mail.ru>
2020-05-08 20:47:55 -0700

    Booking controller is done

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -10,7 +10,46 @@ use App\User;
 
 class BookingController extends Controller
 {
+    public function getBookingForm()
+    {
+        return view('home.room_order');
+    }
 
+
+    public function bookingRoom(Request $req)
+    {
+    	$user = Auth::user();
+    
+
+    	$booking = new Booking([
+    		'arrival' => $req->input('arrival'),
+    		'book_time' => $req->input('book_time'),
+    		'checkout' => $req->input('checkout'),
+    		'breakfast' =>$req->input('breakfast'),
+    		'night' => $req->input('night'),
+    		'comment' => $req->input('comment')
+    	]);
+
+    	$user->bookings()->save($booking);
+
+    	return redirect()->route('Index');
+
+    	// return redirect()->route('Index')->with([
+    	// 	// 'info'=>'Your booked a room successfully! We will wait you in '. $req->input('arrival')
+    	// ]);
+    }
+
+
+
+
+    // For Admins
+    //get
+    public function getBooking()
+    {
+        return view('admin.bookings', [
+            'bookings' => Booking::orderBy('id', 'desc')->get()
+        ]);
+    }
     
     
 }
